5598cf28cd301e5b768eea4460342fed661e71a2
funkeydiscoman <jacksonrfrank@gmail.com>
2017-10-02 11:17:55 -0700

    Finished Cylinder Class

--- Cone.java
+++ Cone.java
@@ -1,13 +1,17 @@
 
 public class Cone extends Cylinder{
+	private float BRadius, height;
+	
 	public Cone(float BRadius, float height) {
 		super(BRadius, height);
+		this.BRadius = BRadius;
+		this.height = height;
 	}
 	
 	public float getVolume() {
 		return (super.getVolume() / 3);
 	}
 	public float getSA() {
-		return()
+		return (float)((Math.PI * BRadius * height) + (Math.PI * BRadius * BRadius));
 	}
 }
