10fad675ae417ac28e29a8d2c1cd77f736d135bc
funkeydiscoman <jacksonrfrank@gmail.com>
2017-10-04 10:55:42 -0700

    Started on main class

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 
 public class Main {
-
+	public enum shape{CONE, CUBE, CYLINDER, RECTPRISM, SPHERE, TRIPRISM}
 	public static void main(String[] args) {
-		System.out.println("3D Shapes");
-
+		System.out.println("3D Shapes!");
+		
+		Interface3D[] shapes = new Interface3D[6];
+		
 	}
 
 }
